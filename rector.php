6910fa6df7a789ecc9f9cf58cba794299a41a316
Sander Muller <github@scode.nl>
2026-04-09 16:20:03 +0200

    chore: add LARAVEL_CODE_QUALITY rector set and bump dev deps

--- rector.php
+++ rector.php
@@ -11,6 +11,7 @@ use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
 use Rector\TypeDeclaration\Rector\ArrowFunction\AddArrowFunctionReturnTypeRector;
 use RectorLaravel\Set\LaravelSetList;
+use RectorPest\Set\PestSetList;
 
 return RectorConfig::configure()
     ->withCache('.cache/rector')
@@ -35,6 +36,13 @@ return RectorConfig::configure()
     ->withPhpSets(php82: true)
     ->withSets([
         LaravelSetList::LARAVEL_110,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
+        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        PestSetList::PEST_CODE_QUALITY,
+        PestSetList::PEST_CHAIN,
+        PestSetList::PEST_LARAVEL,
     ])
     ->withSkip([
         DateFuncCallToCarbonRector::class,
